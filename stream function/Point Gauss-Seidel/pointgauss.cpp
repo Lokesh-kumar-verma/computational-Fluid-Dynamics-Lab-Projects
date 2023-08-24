97bba609cb863362029c17e81a72d23277926e12
Lokesh-kumar-verma <142805969+Lokesh-kumar-verma@users.noreply.github.com>
2023-08-24 17:28:27 +0530

    Create pointgauss.cpp

---
+++ stream function/Point Gauss-Seidel/pointgauss.cpp
@@ -0,0 +1,96 @@
+#include<iostream>
+#include<math.h>
+#include<stdio.h>
+#include<fstream>
+using namespace std;
+int main()
+{
+  int m=31,n=21, i ,j;
+  double B,dx,dy,s;
+  double old[100][100],lat[100][100],le=0,li=0;
+  //dx=0.2;
+  //dy=0.2;
+  dx=6.0/(m-1);
+  dy=4.0/(n-1);
+  B=dx/dy;
+  s=pow(B,2);
+  double l=1/(2*(1+s));
+for(i=0;i<m;i++)
+{
+for (j = 0; j<n; j++)
+  { 
+  if (j == (n-1))
+   {
+    lat[i][j] = 0.0;
+   }
+  else if (i == 0)
+   {
+  lat[i][j] = 0.0;
+   }
+  else if (i <= 5)
+  {
+  lat[i][j] = 0.0;
+  }
+  else if (i >= 6)
+  {
+  lat[i][j] = 100.0;
+  }    
+  else
+  {
+  lat[i][j] = 0.0;
+  }
+ }
+}
+ int iteration = 0; 
+ double error = 1.0; 
+ ofstream out("pointgauss_1_graph.dat");
+ ofstream fp("pointgauss_1.dat");
+ {
+fp<<"VARIABLES=\"X\" \"Y\" \"TEMPERATURE\""<<endl;
+fp<<"ZONE I="<<m<<" J="<<n<<" DATAPACKING=POINT"<<endl;
+out<<"iteration(log10) "<<"\t"<<"error(log10)"<<"\n";
+do
+{
+for (i = 0; i < m; i++)
+ {
+ for (j = 0; j<n; j++)
+  {
+    old[i][j] = lat[i][j];
+  }
+ }
+  for (i = 1; i < (m-1); i++)
+  {
+    for (j = 1; j < (n-1); j++)
+    {
+     lat[i][j]= l*(s*lat[i][j-1]+lat[i-1][j]+old[i+1][j]+s*old[i][j+1]);
+    }
+  }  
+for(j=0;j<n;j++)
+ {
+  lat[m-1][j]=lat[m-2][j];
+ }
+ error=0;
+ for(i=0;i<m;i++)
+ {
+    for(j=0;j<n;j++)
+    {
+        error= error + pow(lat[i][j]-old[i][j],2);
+    }
+ }
+ error=sqrt(error/(m*n));
+ 
+iteration++;
+li=iteration;
+le=log10(error);
+out<<li<<"\t"<<le<<"\n";
+
+} while(error>0.000001); 
+for(i=0;i<m;i++)
+{
+  for(j=0;j<n;j++)
+  fp<<i*dx<<"\t"<<j*dy<<"\t"<<lat[i][j]<<endl;
+}
+ }
+cout<<"no. of iterations"<<iteration;
+return 0;
+}
